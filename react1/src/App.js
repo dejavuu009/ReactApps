6d08ddbf420cc843737028ed24a9146abaa15c26
JakubMacOS <dejavuu009@gmail.com>
2022-11-06 14:21:53 +0100

    InitialArray2

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,40 +1,43 @@
 import React from 'react';
 import ListWrapper from './components/ListWrapper/ListWrapper';
 import './index.css';
+import Form from './components/Form/Form';
 
 
+const initialStateItems = [
+    {
+        image: 'https://i.pinimg.com/originals/ba/d4/5a/bad45a40fa6e153ef8d1599ba875102c.png' ,
+        name: 'Dan Abramov',
+        description: 'Working on @reactjs. The demo guy.',
+        twitterLink: 'https://twitter.com/dan_abramov',
+    },
+    {
+        image: 'https://i.pinimg.com/originals/ba/d4/5a/bad45a40fa6e153ef8d1599ba875102c.png'  ,
+        name: 'Ryan Florence',
+        description: 'Making React accessible for users and developers at https://reach.tech . Online learning, workshops, OSS, and consulting.',
+        twitterLink: 'https://twitter.com/ryanflorence',
+    },
+    {
+        image: 'https://i.pinimg.com/originals/ba/d4/5a/bad45a40fa6e153ef8d1599ba875102c.png'  ,
+        name: 'Michael Jackson',
+        description: 'Maker. Co-author of React Router. Working on @ReactTraining. Created @unpkg. Head over heels for @cari.',
+        twitterLink: 'https://twitter.com/mjackson',
+    },
+    {
+        image: 'https://i.pinimg.com/originals/ba/d4/5a/bad45a40fa6e153ef8d1599ba875102c.png'  ,
+        name: 'Kent C. Dodds',
+        description: 'Making software development more accessible · Husband, Father, Latter-day Saint, Teacher, OSS, GDE, @TC39 · @PayPalEng @eggheadio @FrontendMasters · #JS',
+        twitterLink: 'https://twitter.com/kentcdodds',
+    },
 
+];
 
 
 class App extends React.Component {
 
 state = {
-    items: [
-        {
-            image: 'https://i.pinimg.com/originals/ba/d4/5a/bad45a40fa6e153ef8d1599ba875102c.png' ,
-            name: 'Dan Abramov',
-            description: 'Working on @reactjs. The demo guy.',
-            twitterLink: 'https://twitter.com/dan_abramov',
-        },
-        {
-            image: 'https://i.pinimg.com/originals/ba/d4/5a/bad45a40fa6e153ef8d1599ba875102c.png'  ,
-            name: 'Ryan Florence',
-            description: 'Making React accessible for users and developers at https://reach.tech . Online learning, workshops, OSS, and consulting.',
-            twitterLink: 'https://twitter.com/ryanflorence',
-        },
-        {
-            image: 'https://i.pinimg.com/originals/ba/d4/5a/bad45a40fa6e153ef8d1599ba875102c.png'  ,
-            name: 'Michael Jackson',
-            description: 'Maker. Co-author of React Router. Working on @ReactTraining. Created @unpkg. Head over heels for @cari.',
-            twitterLink: 'https://twitter.com/mjackson',
-        },
-        {
-            image: 'https://i.pinimg.com/originals/ba/d4/5a/bad45a40fa6e153ef8d1599ba875102c.png'  ,
-            name: 'Kent C. Dodds',
-            description: 'Making software development more accessible · Husband, Father, Latter-day Saint, Teacher, OSS, GDE, @TC39 · @PayPalEng @eggheadio @FrontendMasters · #JS',
-            twitterLink: 'https://twitter.com/kentcdodds',
-        },
-    ],
+    items: [...initialStateItems],
+
 }
 
 render() {
@@ -42,6 +45,8 @@ render() {
         <div>
         
         <ListWrapper items={this.state.items}></ListWrapper>
+        <Form></Form>
+        
         
 
         
