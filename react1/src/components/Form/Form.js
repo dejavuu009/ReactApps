eb3835e492b1499cdcd2638bde99831ae2f655a4
JakubMacOS <dejavuu009@gmail.com>
2022-11-22 11:16:31 +0100

    simplest v2 form

--- react1/src/components/Form/Form.js
+++ react1/src/components/Form/Form.js
@@ -4,43 +4,74 @@ import styles from "./Form.module.scss";
 import Title from '../Title/Title';
 
 
+
 const Form = () => {
 
-const [enteredTitle, setTitle] = useState("");
+const [enteredTitle, setEnteredTitle] = useState("");
+const [enteredLink, setEnteredLink] = useState("");
+const [enteredImage, setEnteredImage] = useState("");
+const [enteredDescription, setEnteredDescription] = useState("");
+
+const titleChangeHandler = (event) => {
+  setEnteredTitle(event.target.value)
+}
+const linkChangeHandler = (event) => {
+  setEnteredLink(event.target.value)
+}
+const imageChangeHandler = (event) => {
+  setEnteredImage(event.target.value)
+}
+const descriptionChangeHandler = (event) => {
+  setEnteredDescription(event.target.value)
+}
+
+
 
   const titleChange = (e) => {
-    setTitle(e.target.value)
+    e.preventDefault();
+    
+    const expenseData = {
+      title: enteredTitle,
+      link: enteredLink,
+      image: enteredImage,
+      description: enteredDescription,
+      
+    };
+    console.log(expenseData);
+    setEnteredTitle("");
+    
   };
 
   return(
   <div className={styles.wrapper}>
     <Title>Add new twitter account</Title>
-    <form autoComplete="off" className={styles.form} >
+    <form id="my-form" autoComplete="off" className={styles.form} onSubmit={titleChange} >
     <div>
     <label>Name</label>
-      <input type='text' onChange={titleChange}>
-
+      <input type='text' value={enteredTitle} onChange={titleChangeHandler}  >
+        
       </input>
     </div>
     <div>
     <label>link</label>
-      <input type='text'>
-
+      <input type='text' value={enteredLink} onChange={linkChangeHandler} >
       </input>
     </div>
     <div>
     <label>img</label>
-      <input type='text'>
+      <input type='text' value={enteredImage} onChange={imageChangeHandler} >
 
 
       </input>
     </div>
     <div>
     <label>description</label>
-      <input type='text'>
+      <input type='text'value={enteredDescription} onChange={descriptionChangeHandler} >
 
       </input>
     </div> 
+
+      <button type="submit">click me</button>
     </form>
   </div>
   )
--- react1/src/components/Modal/MyModal.js
+++ react1/src/components/Modal/MyModal.js
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from '../Form/Form'
 
-import { twitterAccounts } from '../../data/twitterAccounts';
+// import { twitterAccounts } from '../../data/twitterAccounts';
 
 
 
@@ -11,7 +11,6 @@ import { twitterAccounts } from '../../data/twitterAccounts';
 const MyModal = () => {
   
 
-  const [data, setData] = useState(twitterAccounts);
 
 
   const [show, setShow] = useState(false);
@@ -20,34 +19,28 @@ const MyModal = () => {
   const handleShow = () => setShow(true);
   
 
-const handleSubmit = () => {
- 
-  handleClose();
-};
 
-const addItem = e => {
-  // e.preventDefault();
 
-  const newItem = {
-    name: e.target[0].value,
-    twitterLink: e.target[1].value,
-    image: e.target[2].value,
-    description: e.target[3].value
-  };
+// const addItem = e => {
+//   // e.preventDefault();
+
+//   const newItem = {
+//     name: e.target[0].value,
+//     twitterLink: e.target[1].value,
+//     image: e.target[2].value,
+//     description: e.target[3].value
+//   };
 
   // this.setState(prevState => ({
   //    ...prevState.items, newItem
   // }));
 
-  setData(newItem)
-  console.log(data)
 
  
 
 
 
-  // e.target.reset();
-};
+
 
   return(
     <>
@@ -69,7 +62,7 @@ const addItem = e => {
         <Button variant="secondary" onClick={handleClose}>
           Close
         </Button>
-        <Button variant="primary" onClick={addItem} >
+        <Button variant="primary" type='submit' form='my-form' onClick={handleClose} >
           Save Changes
         </Button>
       </Modal.Footer>
